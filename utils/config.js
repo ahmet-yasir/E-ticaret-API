4ccab6423e9bee1bf9109c5629680c61514b039b
ahmetyasir <ahmetyasirkulaksiz@gmail.com>
2024-05-12 18:54:24 +0300

    add config.js

---
+++ utils/config.js
@@ -0,0 +1,10 @@
+export const config = {
+    user: 'Username',
+    password: 'password',
+    server: 'servername',
+    database: 'ECommerceDB',
+    options: {
+        enableArithAbort: true,
+        encrypt: false
+    }
+};
